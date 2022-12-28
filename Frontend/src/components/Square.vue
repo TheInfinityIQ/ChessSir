231960b2ce35f91d1adabb0ea01d10fca4cb8f09
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2022-12-27 21:31:05 -0600

    Cursor changes when hovering pieces

--- Frontend/src/components/Square.vue
+++ Frontend/src/components/Square.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-defineProps({
+import { getNotEmptyPieces, getNumNotEmptyPieces } from "@/scripts/staticValues";
+
+let isSelectable = false;
+
+const props = defineProps({
     colour: Number,
     id: Number,
-    piece: String
+    piece: String,
 });
+
+for (let index = 0; index < getNumNotEmptyPieces(); index++) {
+    if (props.piece == getNotEmptyPieces()[index]) {
+        isSelectable = true;
+    }
+}
+
+
 </script>
 
 <template>
-    <div :class="{ lighter: colour == 0, darker: colour == 1, piece } "></div>
+    <div :class="{ lighter: colour == 0, darker: colour == 1, piece, selectable: isSelectable }"></div>
 </template>
 
 <style scoped>
@@ -17,6 +29,10 @@ div {
 
     background-position: center;
     background-size: cover;
+}
+
+.selectable {
+    cursor: grab;
 }
 
 .lighter {
