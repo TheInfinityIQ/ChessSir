e8262e266c9061f46a5194ffc6ddf5cdc8c96065
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2023-01-18 20:16:48 -0600

    Added generalized postSelectedPiece function
    Needed access to props.piece so I passed more info via new function

--- Frontend/src/scripts/state.ts
+++ Frontend/src/scripts/state.ts
@@ -1,31 +1,60 @@
 import { reactive, ref } from "vue";
 
-let idOfSelectedPiece: { id: number } = reactive({ id: -1 });
+let idOfSelectedPiece: number;
+let pieceOfSelectedPiece: string;
+let colourOfSelectedPiece: number;
+
 let isPieceSelected: boolean = false;
+
+// Get Functions
+// --------------------
 
 const getIsPieceSelected = () => {
     return isPieceSelected;
+};
+
+const getIdOfSelectedPiece = () => {
+    return idOfSelectedPiece;
+};
+
+// Value modifying functions
+// --------------------
+
+const postIdOfSelectedPiece = (id: number | undefined) => {
+    if (!id && id != 0) {
+        console.log(
+            "ID of selected piece is invalid. Exiting postIdOfSelectedPiece()"
+        );
+        return;
+    }
+
+    idOfSelectedPiece = id;
+};
+
+const postSelectedPiece = (colour: number, id: number, piece: string, deselect: () => void) => {
+    if (!id && id != 0 || (colour == undefined || piece == undefined)) {
+        console.log(
+            "Piece Prop of selected piece is invalid. Exiting postSelectedPiece()"
+        );
+        return;
+    }
+
+    colourOfSelectedPiece = colour;
+    pieceOfSelectedPiece = piece;
+    idOfSelectedPiece = id;
 };
 
 const updateIsPieceSelected = () => {
     isPieceSelected = !isPieceSelected;
 };
 
-const getIdOfSelectedPiece = () => {
-    return idOfSelectedPiece.id;
-};
-
-const postIdOfSelectedPiece = (id: number | undefined) => {
-    if (!id && id != 0) {
-        return;
-    }
-
-    idOfSelectedPiece = { id };
-};
+// Exports
+// --------------------
 
 export {
     getIdOfSelectedPiece,
     postIdOfSelectedPiece,
     updateIsPieceSelected,
     getIsPieceSelected,
+    postSelectedPiece
 };
