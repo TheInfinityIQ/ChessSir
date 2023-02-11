453a8aeb325a8e33a5c5664e7a267511b7e4d77d
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2023-02-10 22:12:37 -0600

    Added function types for readability

--- Frontend/src/scripts/types.ts
+++ Frontend/src/scripts/types.ts
@@ -1,7 +1,21 @@
+import type { Ref } from "vue";
+
 interface IPiece {
     id: number; 
     colour: number; 
     piece: string
 }
 
-export type { IPiece }
+// No Paramaters
+// --------------------
+type npAny = () => any;
+type npString = () => string;
+type npVoid = () => void;
+
+// One Paramater
+// --------------------
+type boolVoid = (param: boolean) => void;
+type stringVoid = (param: string) => void;
+type refVoid = (param: Ref<any>) => void;
+
+export type { IPiece, npVoid, boolVoid, stringVoid, npAny, npString, refVoid }
