482a4d968be86d53c653e13f3f8374053a34db81
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2023-04-16 12:56:58 -0500

    validateFunctions called when their piece moves
    Pawn move will call validatePawnMoves etc.

--- Frontend/src/scripts/state.ts
+++ Frontend/src/scripts/state.ts
@@ -1,6 +1,7 @@
 import type { Ref } from "vue";
 import { commitMoveToBoard } from "./board";
-import { type refVoid, type IPiece, type npIPiece, Piece, type IMove, Move, type npVoid } from "./types";
+import type { refVoid, IPiece, npIPiece, IMove, npVoid, moveBool } from "./types";
+import { Piece, Move } from "./types";
 
 let selectedSquareId: number | undefined;
 let selectedSquarePiece: string | undefined;
@@ -9,6 +10,15 @@ let selectedSquareColour: number | undefined;
 let pieceRef: Ref<string>;
 
 let deselect: () => void;
+
+enum ChessPiece {
+    PAWN = "p",
+    ROOK = "r",
+    KNIGHT = "n",
+    BISHOP = "b",
+    KING = "k",
+    QUEEN = "q",
+}
 
 // Get Functions
 // --------------------
@@ -72,8 +82,113 @@ const makeMove = (newSquare: IPiece) => {
     }
 
     let move: IMove = new Move(selectedIPiece(), newSquare);
+    validateMove(move); // TODO: update to prevent move from being commited if false
+
     commitMoveToBoard(move);
 };
+
+const validateMove: moveBool = (move: IMove) => {
+    //Call corresponding piece type to validate a move for that piece
+    const pieceType: string = move.fromSquare.piece.substring(move.fromSquare.piece.length, 1);
+    const piece: ChessPiece | undefined = getChessPieceFromLetter(pieceType);
+    let validator: moveBool = () => false;
+
+    if (piece)
+        validator = moveValidators.get(piece)!; // moveValidators will never be invalid if piece is valid
+
+    return validator(move);
+}
+
+const validatePawnMove: moveBool = (move: IMove) => {
+    isMoreThanOneSquare(move); // is only okay if pawn is off starting square
+    isJumpingPiece(move);
+    //More than 2 squares after move one
+    //Backwards
+    //En Passent
+    //Promote
+    console.log("Pawn move validated");
+
+    return false;
+}
+
+const validateRookMove: moveBool = (move: IMove) => {
+    isDiagonalMove(move);
+    isJumpingPiece(move);
+
+    console.log("Rook move validated");
+    return false;
+}
+
+const validateKnightMove: moveBool = (move: IMove) => {
+    isDiagonalMove(move);
+    isVertOrHorizontalMove(move);
+    //Only move in Ls
+
+    console.log("Knight move validated");
+    return false;
+}
+
+const validateBishopMove: moveBool = (move: IMove) => {
+    isVertOrHorizontalMove(move);
+    isJumpingPiece(move);
+
+    console.log("Bishop move validated");
+    return false;
+}
+
+const validateKingMove: moveBool = (move: IMove) => {
+    isMoreThanOneSquare(move);
+    isJumpingPiece(move);
+    //Castles
+
+    console.log("King move validated");
+    return false;
+}
+
+const validateQueenMove: moveBool = (move: IMove) => {
+    isJumpingPiece(move);
+
+    console.log("Queen move validated");
+    return false;
+}
+
+const moveValidators: Map<ChessPiece, moveBool> = new Map([
+    [ChessPiece.PAWN, validatePawnMove],
+    [ChessPiece.ROOK, validateRookMove],
+    [ChessPiece.KNIGHT, validateKnightMove],
+    [ChessPiece.BISHOP, validateBishopMove],
+    [ChessPiece.KING, validateKingMove],
+    [ChessPiece.QUEEN, validateQueenMove],
+]);
+
+const getChessPieceFromLetter = (letter: string): ChessPiece | undefined => {
+    for (const key in ChessPiece) {
+        if (ChessPiece[key as keyof typeof ChessPiece] === letter) {
+            return ChessPiece[key as keyof typeof ChessPiece];
+        }
+    }
+    return undefined;
+};
+
+const isDiagonalMove: moveBool = () => {
+
+    return false;
+}
+
+const isVertOrHorizontalMove: moveBool = () => {
+
+    return false;
+}
+
+const isMoreThanOneSquare: moveBool = () => {
+
+    return false;
+}
+
+const isJumpingPiece: moveBool = () => {
+
+    return false;
+}
 
 // Debug
 // --------------------
