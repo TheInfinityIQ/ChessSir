4ca8ae37e9e1311795074666e99b359ed783d9d5
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2023-05-13 17:45:59 -0500

    test file Rmvd. Unit testing is no longer priority

--- Frontend/src/tests/testPieceRules.ts
+++
@@ -1,4 +0,0 @@
-import { commitCastleToBoard, endOfBoardId, findKing, getSquareWithIdWrapper, getTestBoard, rankAndFileValue, startOfBoardId } from "@/scripts/board";
-import { PieceProps, CastlingPiecesId, ChessPiece, Direction, hasPieceMoved, isMoreThanOneSquare } from "@/scripts/pieceRules";
-import { toggleTurns } from "@/scripts/state";
-import type { IMove, Move } from "@/scripts/types";
