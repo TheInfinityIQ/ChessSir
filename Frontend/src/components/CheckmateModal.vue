b0c5705363426303848d27ab96abb5b1bdc860ac
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2023-05-27 19:14:41 -0500

    Added rough layout of checkmate modal

---
+++ Frontend/src/components/CheckmateModal.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <aside>
+        <h1>LAST PLAYER TO MAKE MOVE WON/LOST</h1>
+        <div>
+            <p>+CALCULATED POINTS - EXISTING RATING</p>
+        </div>
+        <div>
+            opening: {RESULT FROM DB QUERY}[DROP DOWN MENU TO LOOK AT ALL TRANSPOSITIONS AND VARIATIONS] {ICON}
+            total moves: {store.game.board}
+        </div>
+        <button>
+            New Game
+        </button>
+    </aside>
+</template>
+
+<style scoped>
+</style>
